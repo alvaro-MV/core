8a493c3b8d0d983eae1a4b772167793f3b89408c
Álvaro Morales <moralesalvaro97@gmail.com>
2025-03-23 08:08:00 +0100

    Para probar.

--- src/contracts/HM25.h
+++ src/contracts/HM25.h
@@ -1,15 +1,14 @@
-using namespace QPI;  // Interfaz de Qubic
+#include <cstring>
+using namespace QPI;
 
-// Identificador del contrato
-struct ServiceNFT252 { };  // Ejemplo de identificador (nombre arbitrario único)
+struct ServiceNFT252 { };
 
-// Definición del contrato inteligente, heredando de ContractBase
 struct ServiceNFT : public ContractBase {
 public:
     struct AddOffer_input {
         char companyName[64];
         char title[64];
-        char imageRef[128];      // URL o hash de la imagen (ej. IPFS CID)
+        char imageRef[128];
         uint64 price;
         uint32 estimatedTime;
         char description[256];
@@ -51,43 +50,52 @@ private:
     bool offerExists[100];
     uint64 nextOfferId;
 
-    PUBLIC_PROCEDURE(AddOffer)
-        require(qpi.getEntity(), "Caller must be an entity");
-        uint64 id = state.nextOfferId;
-        require(id < 100, "Max offers reached");
+    struct AddOffer_locals {
+        uint64 id;
+    };
 
-        state.offers[id].price = input.price;
-        state.offers[id].estimatedTime = input.estimatedTime;
-        state.offers[id].status = 0;
-        state.offers[id].totalRating = 0;
-        state.offers[id].reviewCount = 0;
-        state.offers[id].owner = qpi.invocator();
+    PUBLIC_PROCEDURE_WITH_LOCALS(AddOffer)
+        qpi.require(qpi.getEntity(qpi.invocator()), "Caller must be an entity");
 
-        memcpy(state.offers[id].companyName, input.companyName, sizeof(input.companyName));
-        memcpy(state.offers[id].title,       input.title,       sizeof(input.title));
-        memcpy(state.offers[id].imageRef,    input.imageRef,    sizeof(input.imageRef));
-        memcpy(state.offers[id].description, input.description, sizeof(input.description));
+        locals.id = state.nextOfferId;
+        qpi.require(locals.id < 100, "Max offers reached");
 
-        state.offerExists[id] = true;
-        state.nextOfferId = id + 1;
+        OfferData& offer = state.offers[locals.id];
+        offer.price = input.price;
+        offer.estimatedTime = input.estimatedTime;
+        offer.status = 0;
+        offer.totalRating = 0;
+        offer.reviewCount = 0;
+        offer.owner = qpi.invocator();
 
-        output.newOfferId = id;
+        std::memcpy(offer.companyName, input.companyName, sizeof(input.companyName));
+        std::memcpy(offer.title,       input.title,       sizeof(input.title));
+        std::memcpy(offer.imageRef,    input.imageRef,    sizeof(input.imageRef));
+        std::memcpy(offer.description, input.description, sizeof(input.description));
+
+        state.offerExists[locals.id] = true;
+        state.nextOfferId = locals.id + 1;
+
+        output.newOfferId = locals.id;
     _
 
     PUBLIC_FUNCTION(GetOffer)
         uint64 id = input.offerId;
         if (id < state.nextOfferId && state.offerExists[id]) {
-            memcpy(output.companyName, state.offers[id].companyName, sizeof(output.companyName));
-            memcpy(output.title,       state.offers[id].title,       sizeof(output.title));
-            memcpy(output.imageRef,    state.offers[id].imageRef,    sizeof(output.imageRef));
-            memcpy(output.description, state.offers[id].description, sizeof(output.description));
-            output.price         = state.offers[id].price;
-            output.estimatedTime = state.offers[id].estimatedTime;
-            output.status        = state.offers[id].status;
-            output.averageRating = (state.offers[id].reviewCount > 0)
-                ? (uint8)(state.offers[id].totalRating / state.offers[id].reviewCount)
+            const OfferData& offer = state.offers[id];
+
+            std::memcpy(output.companyName, offer.companyName, sizeof(output.companyName));
+            std::memcpy(output.title,       offer.title,       sizeof(output.title));
+            std::memcpy(output.imageRef,    offer.imageRef,    sizeof(output.imageRef));
+            std::memcpy(output.description, offer.description, sizeof(output.description));
+
+            output.price         = offer.price;
+            output.estimatedTime = offer.estimatedTime;
+            output.status        = offer.status;
+            output.averageRating = (offer.reviewCount > 0)
+                ? (uint8)(offer.totalRating / offer.reviewCount)
                 : 0;
-            output.reviewCount   = state.offers[id].reviewCount;
+            output.reviewCount   = offer.reviewCount;
         } else {
             output.status = 255;
         }
